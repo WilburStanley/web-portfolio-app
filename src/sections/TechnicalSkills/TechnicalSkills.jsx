58e3f76e9d73c85d107244e36f85b728fe0018e1
Wilbur <wilburstanley.workspace@gmail.com>
2026-04-01 14:25:51 +0800

    Added new item in Dev tools

--- src/sections/TechnicalSkills/TechnicalSkills.jsx
+++ src/sections/TechnicalSkills/TechnicalSkills.jsx
@@ -9,7 +9,7 @@ const technicalSkills = [
     { id : 3, icon : Tablet, title : "Cross-Platform Mobile Development | Android & IOS", categories : ["ReactNative", "Flutter", "Firebase", "SQL"]},
     { id : 4, icon : Monitor, title : "Desktop Development", categories : ["Java Swing", "SQL", "MySQL", "Python Tkinter"]},
     { id : 5, icon : Bot, title : "Automation | Python", categories : ["Web Scrapping", "OpenCV", "Task Automation", "Data Processing"]},
-    { id : 6, icon : Wrench, title : "Developer Tools", categories : ["Github", "VSCode", "Android Studio", "PyCharm", "IntelliJ", "Vercel", "Google Auth (Google Cloud)"]},
+    { id : 6, icon : Wrench, title : "Developer Tools", categories : ["Github", "VSCode", "Android Studio", "PyCharm", "IntelliJ", "Vercel", "Render", "Google Auth (Google Cloud)"]},
 ]
 
 const TechnicalSkills = () => {

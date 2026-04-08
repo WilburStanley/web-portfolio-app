9ed09a7c4f04568c66c1baf6dcd6d80da90e759a
Wilbur <wilburstanley.workspace@gmail.com>
2026-04-08 12:54:30 +0800

    updated brand

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
                 Founder & Creator – Let's Build Something Together
             </div>
             <p className={style.description}>
-                I’m {highlight('Wilbur Stanley Jimenez')}, founder of <strong>{highlight('BrightLabs University')}</strong>. 
+                I’m {highlight('Wilbur Stanley Jimenez')}, founder of <strong>{highlight('BrightLabs')}</strong>. 
                 I build {highlight('custom digital solutions')} for <strong>users</strong>, <strong>creators</strong>, and <strong>business owners</strong> to improve {highlight('everyday workflows')}. 
                 I also create {highlight('quirky apps')} and {highlight('games')} for users. 
                 I am currently <strong>open for freelance projects, collaborations, and full-time opportunities</strong>. 
@@ -28,12 +28,12 @@ const Contact = () => {
                 </a>
 
                 <a
-                    href="https://brightlabs-official-website.vercel.app/#socials" 
+                    href="https://brightlabs-official-website.vercel.app" 
                     target="_blank" 
                     rel="noopener noreferrer"
                     className={`${style.contactButton} ${style.secondaryButton}`} >
                     <Globe size={20} />
-                    Visit BrightLabs University
+                    Visit BrightLabs
                 </a>
             </div>
         </section>
